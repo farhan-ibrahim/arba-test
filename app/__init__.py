79634a69bd0482a96abc07b618f742cc295399f4
Farhan Ibrahim <farhan5543@gmail.com>
2024-10-18 02:54:39 +0800

    add : CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 def create_app():
     from . import models, views, services
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with a strong secret key
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my.db'  # Path to SQLite database
+    CORS(app)
     models.init_app(app)
     views.init_app(app)
     services.init_app(app)
