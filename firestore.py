056faaceb41a82574b9d06eab401334c1d53f312
Farhan Ibrahim <farhan5543@gmail.com>
2025-01-17 11:44:13 +0800

    revert : use default credential

--- firestore.py
+++ firestore.py
@@ -2,7 +2,13 @@ import firebase_admin
 from firebase_admin import firestore, credentials
 from config import GOOGLE_APPLICATION_CREDENTIALS
 
-cred = credentials.Certificate(GOOGLE_APPLICATION_CREDENTIALS)
+# Uncomment to use a service account as credentials
+# Require as service account json file
+
+# cred = credentials.Certificate(GOOGLE_APPLICATION_CREDENTIALS)
+
+# Use the default credentials
+cred = credentials.ApplicationDefault()
 firebase_app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
