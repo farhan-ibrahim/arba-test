18494673cc12220cd37c6202fec75c24f24e3c9c
Farhan Ibrahim <farhan5543@gmail.com>
2025-01-17 11:05:28 +0800

    change : use service account cred

--- firestore.py
+++ firestore.py
@@ -1,7 +1,8 @@
 import firebase_admin
 from firebase_admin import firestore, credentials
+from config import GOOGLE_APPLICATION_CREDENTIALS
 
-cred = credentials.ApplicationDefault()
+cred = credentials.Certificate(GOOGLE_APPLICATION_CREDENTIALS)
 firebase_app = firebase_admin.initialize_app(cred)
 
 db = firestore.client()
